c0cc743ef6e831ed0ebe563333a2a991f4cea69f
mimarben <mimaben@gmail.com>
2025-04-15 11:09:57 +0200

    remove alembic

--- api/alembic/test_connection.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("sqlite:///../../db/economy.db", echo=True)
-connection = engine.connect()
-print("Database connection successful!")
-connection.close()
