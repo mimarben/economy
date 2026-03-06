cff0db002395ceeb4b70f769fb9fa9d6f97a8a92
miguel <mimarben@gmail.com>
2026-03-06 07:42:19 +0100

    feat: Refactor global styling to a Material Dashboard theme and implement a dedicated layout for the login page.

--- client/src/app/core/layout/layout/layout.component.ts
+++ client/src/app/core/layout/layout/layout.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter, Subscription } from 'rxjs';
 
 import { HeaderComponent } from '../header/header.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
@@ -8,7 +10,9 @@ import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     HeaderComponent,
     SideMenuComponent,
@@ -17,10 +21,34 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit, OnDestroy {
   isSideMenuHidden = false;
   pageTitle = '';
+  isLoginPage = false;
+  private routerSubscription?: Subscription;
+
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.checkIfLogin(this.router.url);
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.checkIfLogin(event.urlAfterRedirects);
+        this.setHeaderTitle();
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  private checkIfLogin(url: string) {
+    this.isLoginPage = url.includes('/login');
+  }
+
   setHeaderTitle() {
     let route = this.activatedRoute;
     while (route.firstChild) {
@@ -32,6 +60,4 @@ export class LayoutComponent {
   handleToggleSideMenu() {
     this.isSideMenuHidden = !this.isSideMenuHidden;
   }
-
-
 }
